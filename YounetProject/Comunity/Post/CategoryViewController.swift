65a1b73b8bb66e25a9eebdef3c257dd5e3554bf5
hyeeuncho <heeuncho@naver.com>
2024-01-12 20:03:45 +0900

    Merge

---
+++ YounetProject/Comunity/Post/CategoryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryViewController.swift
+//  YounetProject
+//
+//  Created by 조혠 on 1/11/24.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
