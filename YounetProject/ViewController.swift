b4721eb53cdda7320d6ecdf626ead4df2a5d3395
JeHun Kim <121538666+jehunpfh@users.noreply.github.com>
2024-02-02 21:22:15 +0900

    Revert "NationSelection추가"

--- YounetProject/ViewController.swift
+++ YounetProject/ViewController.swift
@@ -11,8 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        /// 수정내용
+        // Do any additional setup after loading the view.
     }
 
 
