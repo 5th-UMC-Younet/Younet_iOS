7a015e0490ad6f6e544b6f5eb159a842af8846cd
jehunpfh <jehunpfh@gmail.com>
2024-02-04 19:57:11 +0900

    AuthRouter 추가

--- YounetProject/API/APIService.swift
+++ YounetProject/API/APIService.swift
@@ -5,4 +5,23 @@
 //  Created by 김제훈 on 2/4/24.
 //
 
-import Foundation
+import Alamofire
+
+class APIService {
+    static let shared: APIService = APIService()
+    
+    //이메일 코드 전송
+    //이메일 코드 받아오기
+    
+//    func requestEmailCode(email: String, completion: @escaping (String) -> Void){
+//        AF.request(AuthRouter.sendEmailAuthCode(email: email))
+//            .responseDecodable(of: [TodoEntity].self,
+//                                           completionHandler: { response in
+//            //                DataResponse<[TodoEntity], AFError>
+//                            debugPrint(response)
+//                            completion(response.value, response.error)
+//                        })
+//            .res
+//            .responseDecodable(completionHandler: <#T##(DataResponse<Decodable, AFError>) -> Void#>)
+//    }
+}
